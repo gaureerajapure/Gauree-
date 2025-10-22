42bade771c9175ea60281baf31c8a450634be705
gaureerajapure <gaureerajapure1@gmail.com>
2025-10-22 13:53:12 +0530

    Add files via upload
    Program to print palindrome number.

---
+++ gauree.py
@@ -0,0 +1,11 @@
+num=int(input("Enter a number:"))
+temp=num
+reverse=0
+while (num>0):
+    remainder=num%10
+    reverse= reverse * 10 + remainder
+    num=num//10
+if (temp == reverse) :
+        print(f"{temp} is a palindrome number.")
+else:
+        print(f"{temp} is not a palindrome number.")
