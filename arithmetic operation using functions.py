0e0f32e53a21d6c9a1d6bade979ec5038d40f381
gaureerajapure <gaureerajapure1@gmail.com>
2025-11-20 14:52:13 +0530

    Add files via upload

---
+++ arithmetic operation using functions.py
@@ -0,0 +1,11 @@
+def arithmetic(num1,num2):
+    sum=num1+num2
+    print("The sum is:",sum)
+    sub=num1-num2
+    print("The sub is:",sub)
+    prod=num1*num2
+    print("The product is:",prod)
+    divi=num1/num2
+    print("The division is:",divi)
+    return(sum,sub,prod,divi)
+g=arithmetic(100,5)
