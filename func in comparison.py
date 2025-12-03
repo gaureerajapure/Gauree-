004c04ed3634ca021b850775e8b7c8d7231cb0a1
gaureerajapure <gaureerajapure1@gmail.com>
2025-12-03 18:08:59 +0530

    Add files via upload

---
+++ func in comparison.py
@@ -0,0 +1,14 @@
+def integer(num1):
+    if num1>0:
+        print("The number is positive.")
+
+    elif num1<0:
+        print("The number is negative.")
+
+    else:
+        print("The number is zero.")
+    return(num1)
+g=integer(1)
+f=integer(-5)
+k=integer(0)
+
